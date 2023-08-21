90fbebed4bdfd9f1b7a47503a8c5cfcea772caa4
izapcinbprep <142258226+izapcinbprep@users.noreply.github.com>
2023-08-21 11:35:06 -0700

    hallo

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.getElementById("submit-btn");
-    const generateButton = document.getElementById("generate-btn");
     const resultSection = document.getElementById("result");
   
     submitButton.addEventListener("click", function () {
-      const selectedOption = document.querySelector("input[name=q1]:checked");
-      if (selectedOption) {
-        const result = selectedOption.value;
-        resultSection.innerHTML = `<p>Your result: ${result}</p>`;
-      } else {
-        resultSection.innerHTML = `<p>Please select an option.</p>`;
-      }
-    });
-  
-    generateButton.addEventListener("click", function () {
-      const pickUpLines = [
-        "Are you a campfire? Because you’re hot and I want s’more.",
-        "Do you believe in love at first sight, or should I walk by again?",
-        // Add more pick-up lines here
-      ];
-      const randomIndex = Math.floor(Math.random() * pickUpLines.length);
-      const randomLine = pickUpLines[randomIndex];
-      const generatorResult = document.getElementById("generator-result");
-      generatorResult.textContent = randomLine;
+      const result = "Your perfect match is Beef Glerky!";
+      resultSection.innerHTML = `<p>${result}</p>`;
     });
   });  
+  document.addEventListener("DOMContentLoaded", function () {
+    const profileForm = document.getElementById("profile-form");
+    const displayUsername = document.getElementById("display-username");
+    const displayInterests = document.getElementById("display-interests");
+    const displayProfilePic = document.getElementById("display-profile-pic");
+
+    profileForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const interests = document.getElementById("interests").value;
+        // You can handle the profile picture upload here as well
+        
+        displayUsername.textContent = username;
+        displayInterests.textContent = interests;
+        // Set the profile picture source here
+    });
+});
