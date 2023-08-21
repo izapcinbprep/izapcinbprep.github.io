338bb699465b7553f44efabd89caa558f6918cb7
izapcinbprep <142258226+izapcinbprep@users.noreply.github.com>
2023-08-21 11:19:38 -0700

    sup

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
-// Get references to HTML elements
-var submitButton = document.getElementById("submit-btn");
-var resultSection = document.getElementById("result");
-
-// Add event listener to the submit button
-submitButton.addEventListener("click", function() {
-    // Get user's selected answers (You need to implement this)
-    var userAnswers = getSelectedAnswers();
-
-    // Calculate and display the result (You need to implement this)
-    var result = calculateResult(userAnswers);
-    resultSection.innerHTML = "Your match: " + result;
-});
-
-// Functions to be implemented
-function getSelectedAnswers() {
-    // Retrieve user's selected answers from the radio buttons
-    // Return an array or object containing the selected answers
-}
-
-function calculateResult(answers) {
-    // Calculate and return the quiz result based on the user's answers
-}
-// Get references to HTML elements
-var profileForm = document.querySelector(".profile-customization form");
-
-// Add event listener to the profile form submission
-profileForm.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    // Get user inputs (You need to implement this)
-    var username = document.getElementById("username").value;
-    var interests = document.getElementById("interests").value;
-    // ...
-
-    // Save user inputs or update the profile (You need to implement this)
-    saveUserProfile(username, interests);
-});
-// Get references to HTML elements
-var generateButton = document.getElementById("generate-btn");
-var resultDiv = document.getElementById("result");
-
-// List of pick-up lines
-var pickUpLines = [
-    "Are you a Wi-Fi signal? Because I'm feeling a connection.",
-    "Do you have a map? Because I just got lost in your eyes.",
-    "Is your name Google? Because you have everything I've been searching for.",
-    // Add more pick-up lines
-];
-
-// Add event listener to the generate button
-generateButton.addEventListener("click", function() {
-    // Generate a random pick-up line
-    var randomIndex = Math.floor(Math.random() * pickUpLines.length);
-    var randomLine = pickUpLines[randomIndex];
-
-    // Display the pick-up line
-    resultDiv.textContent = randomLine;
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submit-btn");
+    const generateButton = document.getElementById("generate-btn");
+    const resultSection = document.getElementById("result");
+  
+    submitButton.addEventListener("click", function () {
+      const selectedOption = document.querySelector("input[name=q1]:checked");
+      if (selectedOption) {
+        const result = selectedOption.value;
+        resultSection.innerHTML = `<p>Your result: ${result}</p>`;
+      } else {
+        resultSection.innerHTML = `<p>Please select an option.</p>`;
+      }
+    });
+  
+    generateButton.addEventListener("click", function () {
+      const pickUpLines = [
+        "Are you a campfire? Because you’re hot and I want s’more.",
+        "Do you believe in love at first sight, or should I walk by again?",
+        // Add more pick-up lines here
+      ];
+      const randomIndex = Math.floor(Math.random() * pickUpLines.length);
+      const randomLine = pickUpLines[randomIndex];
+      const generatorResult = document.getElementById("generator-result");
+      generatorResult.textContent = randomLine;
+    });
+  });  
